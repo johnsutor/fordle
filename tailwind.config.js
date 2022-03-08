dbe877af72cac53d0028bbb65e4ebc3245de5979
John Sutor <johnsutor3@gmail.com>
2022-03-08 11:56:20 -0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'wordle': ['Clear Sans', 'Helvetica Neue', 'Arial', 'sans-serif'],
+        'karnak': ['karnak'],
+      },
+      colors: {
+        'wbackground': '#121213',
+        'wborder': '#3a3a3c',
+        'wcorrect': '#6aaa64',
+        'wabsent': '#3a3a3c',
+        'wbtnneutral': '#818384',
+        'wbtncorrect': '#548d4e',
+        'wbtnpresent': '#c9b458',
+      }
+    },
   },
   plugins: [],
 }
